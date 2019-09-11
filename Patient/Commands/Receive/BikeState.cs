3335924bdb796ac31c50a50df92cefd65c3f754b
Rick Ladestein <rsladest@avans.nl>
2019-09-11 16:04:31 +0200

    [Added] Bluetooth Support

---
+++ Patient/Commands/Receive/BikeState.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Patient.Commands.Receive
+{
+    enum BikeState
+    {
+        RESERVED = 0x0,
+        ASLEEP = 0x01,
+        READY = 0x02,
+        IN_USE = 0x03
+    }
+}
