8144754206d7e7a9fadd52c6dc77e6a26db18f3b
Yannick van Dolen <ydvd1308@gmail.com>
2019-09-11 16:24:31 +0200

    [Added] Login Form, first part User Select Form

---
+++ Doctor/DocLoginForm.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Doctor
+{
+    public partial class DocLoginForm : Form
+    {
+        public DocLoginForm()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnLogin_Click(object sender, EventArgs e)
+        {
+            if (VerifyLogin(txtUsername.Text, txtPassword.Text))
+            {
+                MessageBox.Show("Hoi " + txtUsername.Text + "!");
+                Form temp = new AstrandDoctorGUI();
+                temp.Show();
+                this.Hide();
+            }
+            else
+            {
+                txtUsername.Clear();
+                txtPassword.Clear();
+                txtUsername.Focus();
+                MessageBox.Show("This username and password combination is incorrect.");
+            }
+        }
+
+        private bool VerifyLogin(string username, string password)
+        {
+            // TODO: Actual security check
+            return true;
+        }
+
+        private void CloseApp()
+        {
+            this.Close();
+        }
+
+        private void DocLoginForm_KeyPress(object sender, KeyPressEventArgs e)
+        {
+
+        }
+    }
+}
