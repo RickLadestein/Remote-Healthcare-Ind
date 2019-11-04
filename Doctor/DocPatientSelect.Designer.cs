ab08be768978d4edbfbfa76f3b3296b331ff7e27
Yannick van Dolen <ydvd1308@gmail.com>
2019-11-04 14:25:50 +0100

    User Select Form

--- Doctor/DocPatientSelect.Designer.cs
+++ Doctor/DocPatientSelect.Designer.cs
@@ -28,81 +28,118 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
+            this.cbxPatientSelect = new System.Windows.Forms.ComboBox();
+            this.btnSelectPatient = new System.Windows.Forms.Button();
+            this.btnNewPatient = new System.Windows.Forms.Button();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.button3 = new System.Windows.Forms.Button();
+            this.btnEditPatient = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // comboBox1
+            // cbxPatientSelect
             // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(12, 12);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(200, 21);
-            this.comboBox1.TabIndex = 0;
+            this.cbxPatientSelect.FormattingEnabled = true;
+            this.cbxPatientSelect.Location = new System.Drawing.Point(12, 12);
+            this.cbxPatientSelect.Name = "cbxPatientSelect";
+            this.cbxPatientSelect.Size = new System.Drawing.Size(200, 21);
+            this.cbxPatientSelect.TabIndex = 0;
             // 
-            // button1
+            // btnSelectPatient
             // 
-            this.button1.Location = new System.Drawing.Point(12, 145);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(200, 34);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "Select Patient";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btnSelectPatient.Location = new System.Drawing.Point(12, 205);
+            this.btnSelectPatient.Name = "btnSelectPatient";
+            this.btnSelectPatient.Size = new System.Drawing.Size(281, 34);
+            this.btnSelectPatient.TabIndex = 1;
+            this.btnSelectPatient.Text = "Select Patient";
+            this.btnSelectPatient.UseVisualStyleBackColor = true;
             // 
-            // button2
+            // btnNewPatient
             // 
-            this.button2.Location = new System.Drawing.Point(108, 223);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 2;
-            this.button2.Text = "New...";
-            this.button2.UseVisualStyleBackColor = true;
+            this.btnNewPatient.Location = new System.Drawing.Point(218, 11);
+            this.btnNewPatient.Name = "btnNewPatient";
+            this.btnNewPatient.Size = new System.Drawing.Size(75, 23);
+            this.btnNewPatient.TabIndex = 2;
+            this.btnNewPatient.Text = "New...";
+            this.btnNewPatient.UseVisualStyleBackColor = true;
             // 
             // groupBox1
             // 
-            this.groupBox1.Controls.Add(this.button3);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.btnEditPatient);
             this.groupBox1.Location = new System.Drawing.Point(12, 39);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(200, 100);
+            this.groupBox1.Size = new System.Drawing.Size(281, 160);
             this.groupBox1.TabIndex = 3;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "groupBox1";
             // 
-            // button3
+            // btnEditPatient
             // 
-            this.button3.Location = new System.Drawing.Point(56, 64);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 23);
-            this.button3.TabIndex = 0;
-            this.button3.Text = "Edit";
-            this.button3.UseVisualStyleBackColor = true;
+            this.btnEditPatient.Location = new System.Drawing.Point(200, 131);
+            this.btnEditPatient.Name = "btnEditPatient";
+            this.btnEditPatient.Size = new System.Drawing.Size(75, 23);
+            this.btnEditPatient.TabIndex = 0;
+            this.btnEditPatient.Text = "Edit";
+            this.btnEditPatient.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(25, 47);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Gender";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(25, 79);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(26, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Age";
             // 
             // DocPatientSelect
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(300, 248);
             this.Controls.Add(this.groupBox1);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.btnNewPatient);
+            this.Controls.Add(this.btnSelectPatient);
+            this.Controls.Add(this.cbxPatientSelect);
             this.Name = "DocPatientSelect";
             this.Text = "DocPatientSelect";
             this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.ComboBox comboBox1;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ComboBox cbxPatientSelect;
+        private System.Windows.Forms.Button btnSelectPatient;
+        private System.Windows.Forms.Button btnNewPatient;
         private System.Windows.Forms.GroupBox groupBox1;
-        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button btnEditPatient;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
     }
 }
