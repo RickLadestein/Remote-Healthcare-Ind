1eb4bc92955f26592ad00eaa181a56ddbc0925f9
Yannick van Dolen <ydvd1308@gmail.com>
2020-01-07 13:40:00 +0100

    Moved event functions to the right file

--- Patient/PatPatientSelect.Designer.cs
+++ Patient/PatPatientSelect.Designer.cs
@@ -121,35 +121,6 @@ namespace Patient
 
         }
 
-        private void PatPatientSelect_FormClosed(object sender, FormClosedEventArgs e)
-        {
-            throw new NotImplementedException();
-        }
-
-        private void BtnEditPatient_Click(object sender, EventArgs e)
-        {
-            throw new NotImplementedException();
-        }
-
-        private void BtnNewPatient_Click(object sender, EventArgs e)
-        {
-            throw new NotImplementedException();
-        }
-
-        private void BtnSelectPatient_Click(object sender, EventArgs e)
-        {
-            throw new NotImplementedException();
-        }
-
-        private void CbxPatientSelect_TextChanged(object sender, EventArgs e)
-        {
-            throw new NotImplementedException();
-        }
-
-        private void cbxPatientSelect_SelectedIndexChanged(object sender, EventArgs e)
-        {
-            throw new NotImplementedException();
-        }
 
         #endregion
 
--- Patient/PatPatientSelect.cs
+++ Patient/PatPatientSelect.cs
@@ -16,5 +16,37 @@ namespace Patient
         {
             InitializeComponent();
         }
+
+
+
+        private void PatPatientSelect_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void BtnEditPatient_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void BtnNewPatient_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void BtnSelectPatient_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void CbxPatientSelect_TextChanged(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void cbxPatientSelect_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
